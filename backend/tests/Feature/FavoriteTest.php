64ccff4a147248a305752d089a8d9cdd3553f910
Alex Bleotu <alexbleotu2006@gmail.com>
2025-02-01 14:55:38 +0200

    Working Serve

--- backend/tests/Feature/FavoriteTest.php
+++ backend/tests/Feature/FavoriteTest.php
@@ -60,7 +60,7 @@ class FavoriteTest extends TestCase
         
         $this->user->favorites()->attach($this->recipe->id);
         
-        $response = $this->actingAs($this->user)->getJson("/api/recipes/favorites/index");
+        $response = $this->actingAs($this->user)->getJson("/api/recipes/favorites");
 
         $response->assertStatus(200)
                  ->assertJsonStructure(['data' => [['id', 'title', 'description']]]);
@@ -75,7 +75,7 @@ class FavoriteTest extends TestCase
         $response = $this->deleteJson("/api/recipes/favorites/{$this->recipe->id}");
         $response->assertStatus(401);
 
-        $response = $this->getJson("/api/recipes/favorites/index");
+        $response = $this->getJson("/api/recipes/favorites");
         $response->assertStatus(401);
     }
 }
