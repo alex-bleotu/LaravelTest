0dd2966ae96b8303663109d0382f453c3f6e26c7
Alex Bleotu <alexbleotu2006@gmail.com>
2025-02-01 15:18:17 +0200

    Config?

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -62,13 +62,13 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ],
     ],
 
     /*
